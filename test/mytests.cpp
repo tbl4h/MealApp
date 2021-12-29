019fc2ce4a914b342aec019aba8892eb2348b617
Michal <mic.maci2891@gmail.com>
2021-12-29 21:32:49 +0100

    Add single test-case for all expected Meal constructor argument.

--- test/mytests.cpp
+++ test/mytests.cpp
@@ -5,8 +5,20 @@
 
 TEST(Meal_class, Meal_class_constructor)
 {    
-    Meal myMeal(Meals::Rice_and_chicken);
-    EXPECT_THAT(myMeal.get_name(),"Rice and chicken");    
+    Meal myMeal1(Meals::Spagetti);
+    Meal myMeal2(Meals::Pizza);
+    Meal myMeal3(Meals::Fish_and_Chips);
+    Meal myMeal4(Meals::Rice_and_chicken);
+    Meal myMeal5(Meals::Salmon);
+    Meal myMeal6(Meals::Roasted_beef);
+    Meal myMeal7(Meals::Sushi);
+    EXPECT_THAT(myMeal1.get_name(),"Spagetti");
+    EXPECT_THAT(myMeal2.get_name(),"Pizza");
+    EXPECT_THAT(myMeal3.get_name(),"Fish and Chips");
+    EXPECT_THAT(myMeal4.get_name(),"Rice and chicken");
+    EXPECT_THAT(myMeal5.get_name(),"Salmon");
+    EXPECT_THAT(myMeal6.get_name(),"Roasted beef");
+    EXPECT_THAT(myMeal7.get_name(),"Sushi");    
 }
 TEST(Meal_class, Meal_class_constructor_argument_is_out_of_range){
     EXPECT_THROW(Meal _meal(static_cast<Meals>(10)),std::out_of_range); 
@@ -16,8 +28,7 @@ TEST(Rolling_function, roll_in_Meals_range)
     for(int i=0;i<100;i++){
         Meals meal = roll();    
         EXPECT_NO_THROW(Meal _meal(meal));
-    }
-      
+    }      
 }
 int main(int argc, char* argv[])
 {
