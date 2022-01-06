37cb44158a62991543bc4e76c7ab7025a3269cf6
Michal <mic.maci2891@gmail.com>
2022-01-07 00:37:17 +0100

    Test commit from pc terminal.

---
+++ build/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/test/mytests.cpp.o"
+  "bin/tests"
+  "bin/tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
