d42416cee28e90b7a3f35471970c044a74fda404
tbl4h <45106120+tbl4h@users.noreply.github.com>
2022-01-07 14:02:30 +0100

    Add files via upload

--- sources/meals_list.cpp
+++ sources/meals_list.cpp
@@ -48,8 +48,8 @@ namespace MealsList
         }
         _impl->_Sql = "CREATE TABLE MEALS("
                       "NAME           TEXT   PRIMARY KEY  NOT NULL,"
-                      "INGREDIENTS    TEXT    "
-                      "PREPARATION_TIME INT,"
+                      "INGREDIENTS    TEXT,"
+                      "PREPARATION_TIME     INT,"
                       "COOKING_TIME   INT,"
                       "DIFFICULTY     TEXT,"
                       "PORTIONS       INT,"
@@ -64,7 +64,7 @@ namespace MealsList
         if (_impl->_Rc != SQLITE_OK)
         {
             std::cerr << "Table can't be created.\n"
-                      << _impl->_ZErrMsg;
+                      << _impl->_ZErrMsg << std::endl;
             sqlite3_free(_impl->_ZErrMsg);
         }
         else
@@ -124,26 +124,31 @@ namespace MealsList
         {
 
             std::stringstream my_stream;
-            my_stream << "INSERT IGNORE INTO MEALS (NAME, INGREDIENTS, PREPARATION_TIME, COOKING_TIME, DIFFICULTY, PORTIONS, KCAL, FAT, CARBS, PROTEIN, STEPS) ";
+            my_stream << "INSERT OR IGNORE INTO MEALS (`NAME`, `INGREDIENTS`, `PREPARATION_TIME`, `COOKING_TIME`, `DIFFICULTY`, `PORTIONS`, `KCAL`, `FAT`, `CARBS`, `PROTEIN`, `STEPS`) ";
             my_stream << "VALUES (";
-            my_stream << "\'";
+            my_stream << "'";
             my_stream << Meal.first;
-            my_stream << "\'";
+            my_stream << "'";
             my_stream << ", ";
+            if (Meal.second.ingredients.empty())
+            {
+                my_stream << "'";                
+                my_stream << "'";
+            }
             for (auto Ingredient : Meal.second.ingredients)
             {
-                my_stream << "\'";
+                my_stream << "'";
                 my_stream << Ingredient;
-                my_stream << "\'";
+                my_stream << "'";
             }
             my_stream << ", ";
             my_stream << Meal.second.prepTime;
             my_stream << ", ";
             my_stream << Meal.second.cookTime;
             my_stream << ", ";
-            my_stream << "\'";
+            my_stream << "'";
             my_stream << Meal.second.difficulty;
-            my_stream << "\'";
+            my_stream << "'";
             my_stream << ", ";
             my_stream << Meal.second.portions;
             my_stream << ", ";
@@ -155,7 +160,7 @@ namespace MealsList
             my_stream << ", ";
             my_stream << Meal.second.protein;
             my_stream << ", ";
-            my_stream << "\'";
+            my_stream << "'";
             for (auto Steps : Meal.second.steps)
             {
                 my_stream << Steps.first;
@@ -164,9 +169,21 @@ namespace MealsList
                 // change it
                 my_stream << "nn";
             }
-            my_stream << "\'";
+            my_stream << "'";
             my_stream << " );";
-            _impl->_Sql = my_stream.str();
+            _impl->_Sql = my_stream.str();            
+            _impl->_Rc = sqlite3_exec(_impl->_Db, _impl->_Sql.c_str(), MealsList::my_special_callback, NULL, &_impl->_ZErrMsg);
+
+            if (_impl->_Rc != SQLITE_OK)
+            {
+                std::cerr << "Can't add values to database: \n"
+                          << _impl->_ZErrMsg << std::endl;
+                sqlite3_free(_impl->_ZErrMsg);
+            }
+            else
+            {
+                std::cerr << "Add value successfully.\n";
+            }
         }
     }
 }
